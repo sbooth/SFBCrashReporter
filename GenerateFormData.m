58eeab92dd8f1b609c6d4ba85b17de9a7836fc3d
Stephen F. Booth <me@sbooth.org>
2009-04-17 21:40:25 -0700

    Use UTF8 as the transfer charset

--- GenerateFormData.m
+++ GenerateFormData.m
@@ -23,27 +23,32 @@ GenerateFormData(NSDictionary *formValues, NSString *boundary)
 		// String value
 		if([value isKindOfClass:[NSString class]]) {
 			NSString *string = (NSString *)value;
-			[result appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"Content-Type: text/plain; charset=utf-8\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 			[result appendData:[[string stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] dataUsingEncoding:NSUTF8StringEncoding]];
 		}
 		// Number value
 		else if([value isKindOfClass:[NSNumber class]]) {
 			NSNumber *number = (NSNumber *)value;
-			[result appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"Content-Type: text/plain; charset=utf-8\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 			[result appendData:[[[number stringValue] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] dataUsingEncoding:NSUTF8StringEncoding]];
 		}
 		// URL value (only file URLs are supported)
 		else if([value isKindOfClass:[NSURL class]] && [(NSURL *)value isFileURL]) {
 			NSURL *url = (NSURL *)value;
 			[result appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n", key, [[url path] lastPathComponent]] dataUsingEncoding:NSUTF8StringEncoding]];
-			[result appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"Content-Type: application/octet-stream\r\n\r" dataUsingEncoding:NSUTF8StringEncoding]];
+			[result appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 			[result appendData:[NSData dataWithContentsOfURL:url]];
 		}
 		// Illegal class
 		else
-			NSLog(@"SFBCrashReporterError: formValues contained illegal object %@", value);
+			NSLog(@"SFBCrashReporterError: formValues contained illegal object %@ of class %@", value, [value class]);
 		
-		[result appendData:[[NSString stringWithString:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+		[result appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 	}
 	
 	[result appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
